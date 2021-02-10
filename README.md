97382bef22abca86fea0ce2431b6a8919f5d8cb6
Sitkus <65811246+Sitkus@users.noreply.github.com>
2021-02-10 13:11:26 +0200

    Created client side with Vue cli

---
+++ README.md
@@ -0,0 +1,21 @@
+# Microposts
+
+Project is built for learning purposes. I wanted to learn and practise how front-end gets connected with back-end, specifically Vue with Node and some Database, in this case it's MongoDB.
+
+## Quick Start
+
+```bash
+# Install dependencies
+npm install
+
+# Start Express Server: http://localhost:5000
+npm start
+
+# Start Vue DevServer: http://localhost:8080
+cd client
+npm run serve
+
+# Build for production (Will build into server/public, ready for deployment)
+cd client
+npm run build
+```
---
+++ server/routes/api/posts.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongodb = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+
+  res.send(await posts.find({}).toArray());
+});
+
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  const validDescription =
+    typeof req.body.description === 'string' && req.body.description.length > 0 && req.body.description.length < 100;
+
+  if (validDescription) {
+    await posts.insertOne({
+      description: req.body.description,
+      createdAt: new Date()
+    });
+
+    res.status(201).send();
+  } else {
+    res.status(400).send('Error: Please insert description');
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  const properId = ObjectID.isValid(req.params.id);
+
+  if (properId) {
+    await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+
+    res.status(200).send('Post deleted successfully');
+  } else {
+    res.status(400).send('Incorrect ID provided');
+  }
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://lukas:sitkus@posts.l2cvf.mongodb.net/microposts-vue-node?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+
+  return client.db('microposts-vue-node').collection('posts');
+}
+
+module.exports = router;
